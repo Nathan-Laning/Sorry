88557c833223783b8a8c5c250ec90ac64e7c8d87
Nathan Laning <nlaning@uvm.edu>
2018-04-04 18:18:45 -0400

    new up

--- Main.java
+++ Main.java
@@ -1,76 +1,59 @@
-public class Main extends Display{
-//    static Display DISPLAY = new Display();
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Main extends Display {
+    //    static Display DISPLAY = new Display();
     public static void main(String[] args) {
-        testing();
+        new mainMenu();
     }
+}
+class mainMenu extends Main{
 
-    public static void testing() {
-        gameBoard g = new gameBoard();
+    mainMenu(){
+        clickSpace newGame = new clickSpace(3425, 665, 725, 2120);
+        clickSpace loadGame = new clickSpace(3425, 665, 725, 2785);
+        clickSpace rules = new clickSpace(3425, 665, 725, 2785 + 665);
+        image StartingPage = new image("Sorry-splash.jpg");
+        newGame.button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                newGame();
+                loadGame.disable();
+                rules.disable();
+                newGame.disable();
+            }
+        });
+        loadGame.button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                loadGame();
+                loadGame.disable();
+                rules.disable();
+                newGame.disable();
+            }
+        });
+        rules.button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                Rules();
+                loadGame.disable();
+                rules.disable();
+                newGame.disable();
+            }
+        });
+        }
+        private void newGame(){
+            //just displaying gameboard for fun
+            //start new Game, however we want to do that
+            image GAMEBOARD=new image("Sorry-board.jpg");
+        }
+        private void Rules(){
+            //open rules page
+            //allow exit
+            //re-instantiate game if started
+        }
+        private void loadGame(){
+            //figure out load game dialogue
 
-        //        Window w = new Window();
-
-//        Display.image i = DISPLAY.new image("Sorry-pawns.png",4,2);
-//        Display.image e = d.new image("Sorry-board.jpg");
-//        Display.image a = d.new image("Sorry-pawns.png",4,2);
-//        Display.image b = d.new image("Sorry-pawns.png",4,1);
-//        Display.image c = d.new image("Sorry-pawns.png",4,3);
-//        Display.image f = d.new image("Sorry-pawns.png",4,4);
-
-
-//
-//        i.move(50,50,1);
-//        a.hide();
-//        b.move(50,40,1);
-//        c.move(50,10,1);
-//        f.move(50,90,1);
-//        a.show();
-
-//        e.move(10,10,1);
-//        j.move(0,0,1);
-
-//        image board = new Display.image("Sorry-board.jpg");
-//        pawn.init();
-//        pawn.move(100,100);
-//        image back = new image("sorry-board.jpg");
-
-//        JLabel splash = loadImage("sorry-Splash.jpg");
-//        JLabel back = loadImage("sorry-board.jpg");
-//        JLabel label2 = loadImage("Sorry-cards.png",12,1);
-//        JLabel label1 = loadImage("Sorry-pawns.png",4,2);
-//        JLabel pawn2 = loadImage("Sorry-pawns.png",4,4);
-//        JPanel panel = new JPanel(null);
-//        panel.add(splash);
-//        panel.add(pawn2);
-//        panel.add(label2);
-//        panel.add(label1);
-//        panel.add(back);
-//        label1.setLocation(60,60);
-//        label2.setLocation(400,500);
-//        splash.setLocation(0,0);
-//        try {
-//            Thread.sleep(3000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-
-//        frame.getContentPane().add(new JLabel(new ImageIcon(loadImage("Sorry-pawns.png",100,100))));
-//        frame.add(panel);
-//        frame.setSize(screenSize);
-//        frame.setVisible(true);
-////        splash.setLocation(dimension,dimension);
-//        for (int i = 0; i < dimension/2+6; i++) {
-//            try {
-//                Thread.sleep(refreshRate);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            label1.setLocation(16,i);
-//            pawn2.setLocation(i,16);
-//        }
-
-
-    }
-
-
-
+        }
 }
